d104e1a88784373c3cf444ec9664b8a250d38f74
Sore Wa Nya <sorewanya@gmail.com>
2025-06-27 18:13:58 +0400

    fastHash function

---
+++ lib/core/hash_func.dart
@@ -0,0 +1,16 @@
+///easy hash function
+///used in SettingsEntity to create `int id` from `name`
+int fastHash(String string) {
+  var hash = 0xcbf29ce484222325;
+
+  var i = 0;
+  while (i < string.length) {
+    final codeUnit = string.codeUnitAt(i++);
+    hash ^= codeUnit >> 8;
+    hash *= 0x100000001b3;
+    hash ^= codeUnit & 0xFF;
+    hash *= 0x100000001b3;
+  }
+
+  return hash;
+}
