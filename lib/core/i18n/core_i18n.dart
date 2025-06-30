cebce489baac24d46ff75bfa98a4eb865a399b95
Sore Wa Nya <sorewanya@gmail.com>
2025-06-30 13:37:22 +0400

    start add I18n

---
+++ lib/core/i18n/core_i18n.dart
@@ -0,0 +1,88 @@
+// dart format width=999
+
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+
+part 'core_ru.dart';
+
+///
+/// Internationalization (i18n) strings for the Tomato app.
+///
+/// (you can use packages [easy_localization], [localization] or [slang] and make implementation of this class for your lang)
+///
+class CoreI18n {
+  String get serverFailureMessage => "Server failure! Try again in 5 seconds.";
+  String get emptyLocalStorageFailureMessage => "Nothing found!";
+  String get cachedFailureMessage => "Cache failure! Try again in 5 seconds.";
+  String descriptionCoreSettingsEnum(CoreSettingsEnum setting) => switch (setting) {
+    CoreSettingsEnum.viewDefault => "Default page to open",
+    CoreSettingsEnum.showDeleted => "Show deleted items? global setting",
+    CoreSettingsEnum.themeMode => "Light/dark theme",
+    CoreSettingsEnum.autoSaveOnPop => "Save item on exit without prompt?",
+    CoreSettingsEnum.allAfterSaveItemShowInfobar => "Show message after saving?",
+    CoreSettingsEnum.allAfterRemoveItemReloadList => "Automatically refresh search results after deleting an item?",
+    CoreSettingsEnum.searchUpdateListAfterReset => "Update search immediately when any field is cleared, not by 'Search' button",
+    CoreSettingsEnum.plusIntValues => "Numbers for increment buttons",
+    CoreSettingsEnum.searchCaseSensitive => "Case-sensitive search? False - search is NOT case-sensitive",
+    CoreSettingsEnum.datetimeDefaultPersonalFormat => "Date display, custom option, overrides ${CoreSettingsEnum.datetimeDefaultFormat}",
+    CoreSettingsEnum.datetimeDefaultFormat => "Date display, suggested options",
+    CoreSettingsEnum.datetimeLanguage => "Date display based on ru/en style",
+    CoreSettingsEnum.searchAddParentToChildList => "Add parent when searching for item's children?",
+    CoreSettingsEnum.globalLoggingEnable => "Enable logging, globally?",
+    CoreSettingsEnum.globalLoggingSizeLimitType => "Log size limit type,\nbyItem - N log entries are saved for each item.\nbyClass - N entries are saved for each data type\nwhere N is the value from ${CoreSettingsEnum.globalLoggingSizeLimitCount}",
+    CoreSettingsEnum.globalLoggingSizeLimitCount => "Log size, see ${CoreSettingsEnum.globalLoggingSizeLimitType}",
+    CoreSettingsEnum.globalSwipeBaseList => "Swipe reaction options in the app window",
+    CoreSettingsEnum.globalSwipeLeftToRight => "Reaction options for right direction",
+    CoreSettingsEnum.globalSwipeRightToLeft => "Reaction options for left direction",
+    CoreSettingsEnum.globalSwipeTopToDown => "Reaction options for down direction",
+    CoreSettingsEnum.globalSwipeDownToTop => "Reaction options for up direction",
+    CoreSettingsEnum.itemSwipeBaseList => "Item swipe reaction options",
+    CoreSettingsEnum.globalQuery => "Global stamp",
+    CoreSettingsEnum.globalTr => "Transaction",
+  };
+  String descriptionSettingsSettingsEnum(SettingsSettingsEnum setting) => switch (setting) {
+    SettingsSettingsEnum.viewDefault => "Settings list view as list or tree",
+    SettingsSettingsEnum.loggingEnable => "Settings changes logging",
+    SettingsSettingsEnum.savedSearch => "Saved search queries",
+    SettingsSettingsEnum.typesNames => "Settings types as list",
+    SettingsSettingsEnum.version => "Stores the last version for ",
+    SettingsSettingsEnum.itemSwipeLeftToRight => "Reaction options for right direction",
+    SettingsSettingsEnum.itemSwipeRightToLeft => "Reaction options for left direction",
+  };
+  String descriptionIconSettingsEnum(IconSettingsEnum setting) => switch (setting) {
+    //TODO
+    IconSettingsEnum.settingsItem => "Icon for ",
+    IconSettingsEnum.settingsList => "Icon for ",
+    IconSettingsEnum.settingsResetToDefault => "Icon for ",
+    IconSettingsEnum.userValue => "Icon for ",
+    IconSettingsEnum.defaultValue => "Icon for ",
+    IconSettingsEnum.info => "Icon for ",
+    IconSettingsEnum.close => "Icon for ",
+    IconSettingsEnum.searchSearch => "Icon for ",
+    IconSettingsEnum.searchReset => "Icon for ",
+    IconSettingsEnum.add => "Icon for ",
+    IconSettingsEnum.save => "Icon for ",
+    IconSettingsEnum.delete => "Icon for ",
+    IconSettingsEnum.restore => "Icon for ",
+    IconSettingsEnum.less => "Icon for ",
+    IconSettingsEnum.lessOrEqual => "Icon for ",
+    IconSettingsEnum.greater => "Icon for ",
+    IconSettingsEnum.greaterOrEqual => "Icon for ",
+    IconSettingsEnum.equal => "Icon for ",
+    IconSettingsEnum.notEqual => "Icon for ",
+    IconSettingsEnum.trueIcon => "Icon for ",
+    IconSettingsEnum.falseIcon => "Icon for ",
+    IconSettingsEnum.setTimeData => "Icon for ",
+    IconSettingsEnum.dropDown => "Icon for ",
+    IconSettingsEnum.dropUp => "Icon for ",
+    IconSettingsEnum.goBack => "Icon for ",
+    IconSettingsEnum.textShort => "Icon for ",
+    IconSettingsEnum.textMidl => "Icon for ",
+    IconSettingsEnum.textFull => "Icon for ",
+    IconSettingsEnum.textField => "Icon for ",
+    IconSettingsEnum.slider => "Icon for ",
+    IconSettingsEnum.themeLight => "Icon for ",
+    IconSettingsEnum.themeDark => "Icon for ",
+    IconSettingsEnum.themeSystem => "Icon for ",
+    IconSettingsEnum.import => "Icon for import",
+  };
+}
---
+++ lib/core/i18n/core_ru.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: annotate_overrides
+// dart format width=999
+
+part of 'core_i18n.dart';
+
+class CoreI18nRu implements CoreI18n {
+  String get serverFailureMessage => "Ошибка сервера! Повтор попытки каждые 5 сек.";
+  String get emptyLocalStorageFailureMessage => "Ничего не найдено!";
+  String get cachedFailureMessage => "Ошибка локальной БД. Повтор попытки каждые 5 сек.";
+  String descriptionCoreSettingsEnum(CoreSettingsEnum setting) => switch (setting) {
+    CoreSettingsEnum.viewDefault => "Открываемая по умолчанию страница",
+    CoreSettingsEnum.showDeleted => "Отображать удалённые элементы? глобальная настройка",
+    CoreSettingsEnum.themeMode => "Светлая/темная тема",
+    CoreSettingsEnum.autoSaveOnPop => "Сохранять элемент при выходе без запроса?",
+    CoreSettingsEnum.allAfterSaveItemShowInfobar => "Показывать сообщении после сохранения?",
+    CoreSettingsEnum.allAfterRemoveItemReloadList => "Автоматически обновлять результаты поиска после удаления элемента?",
+    CoreSettingsEnum.searchUpdateListAfterReset => "Обновлять поиск сразу при обнулении любого поля, а не по кнопке 'Найти'",
+    CoreSettingsEnum.plusIntValues => "Числа для кнопок прибавления",
+    CoreSettingsEnum.searchCaseSensitive => "Регистрозависимый поиск? False - поиск НЕ регистро-зависим",
+    CoreSettingsEnum.datetimeDefaultPersonalFormat => "Отображение даты, пользовательский вариант, перекрывает ${CoreSettingsEnum.datetimeDefaultFormat}",
+    CoreSettingsEnum.datetimeDefaultFormat => "Отображение даты, предложенные варианты",
+    CoreSettingsEnum.datetimeLanguage => "Отображение даты исходя из ru/en стилистики",
+    CoreSettingsEnum.searchAddParentToChildList => "Добавить родителя при поиске наследников элемента?",
+    CoreSettingsEnum.globalLoggingEnable => "Включить логирование, глобально?",
+    CoreSettingsEnum.globalLoggingSizeLimitType => "Тип ограничения логов,\nbyItem - сохраняется N записей логов для каждого элемента.\nbyClass - сохраняется N записей для каждого типа данных\nгде N - значение из ${CoreSettingsEnum.globalLoggingSizeLimitCount}",
+    CoreSettingsEnum.globalLoggingSizeLimitCount => "Размер логов, см. ${CoreSettingsEnum.globalLoggingSizeLimitType}",
+    CoreSettingsEnum.globalSwipeBaseList => "Варианты реакций на свайп в окне приложения",
+    CoreSettingsEnum.globalSwipeLeftToRight => "Варианты реакций для направления вправо",
+    CoreSettingsEnum.globalSwipeRightToLeft => "Варианты реакций для направления влево",
+    CoreSettingsEnum.globalSwipeTopToDown => "Варианты реакций для направления вниз",
+    CoreSettingsEnum.globalSwipeDownToTop => "Варианты реакций для направления вверх",
+    CoreSettingsEnum.itemSwipeBaseList => "Варианты реакций на свайп элемента",
+    CoreSettingsEnum.globalQuery => "Глобальный штамп",
+    CoreSettingsEnum.globalTr => "Транзакция",
+  };
+  String descriptionSettingsSettingsEnum(SettingsSettingsEnum setting) => switch (setting) {
+    SettingsSettingsEnum.viewDefault => "Отображение списка настроек в виде списка или дерева",
+    SettingsSettingsEnum.loggingEnable => "Логирование изменений настроек",
+    SettingsSettingsEnum.savedSearch => "Сохранённые поисковые запросы",
+    SettingsSettingsEnum.typesNames => "Типы настроек в виде списка",
+    SettingsSettingsEnum.version => "Хранит последнюю версию для ",
+    SettingsSettingsEnum.itemSwipeLeftToRight => "Варианты реакций для направления вправо",
+    SettingsSettingsEnum.itemSwipeRightToLeft => "Варианты реакций для направления влево",
+  };
+
+  String descriptionIconSettingsEnum(IconSettingsEnum setting) => switch (setting) {
+    //TODO
+    IconSettingsEnum.settingsItem => "Иконка для ",
+    IconSettingsEnum.settingsList => "Иконка для ",
+    IconSettingsEnum.settingsResetToDefault => "Иконка для ",
+    IconSettingsEnum.userValue => "Иконка для ",
+    IconSettingsEnum.defaultValue => "Иконка для ",
+    IconSettingsEnum.info => "Иконка для ",
+    IconSettingsEnum.close => "Иконка для ",
+    IconSettingsEnum.searchSearch => "Иконка для ",
+    IconSettingsEnum.searchReset => "Иконка для ",
+    IconSettingsEnum.add => "Иконка для ",
+    IconSettingsEnum.save => "Иконка для ",
+    IconSettingsEnum.delete => "Иконка для ",
+    IconSettingsEnum.restore => "Иконка для ",
+    IconSettingsEnum.less => "Иконка для ",
+    IconSettingsEnum.lessOrEqual => "Иконка для ",
+    IconSettingsEnum.greater => "Иконка для ",
+    IconSettingsEnum.greaterOrEqual => "Иконка для ",
+    IconSettingsEnum.equal => "Иконка для ",
+    IconSettingsEnum.notEqual => "Иконка для ",
+    IconSettingsEnum.trueIcon => "Иконка для ",
+    IconSettingsEnum.falseIcon => "Иконка для ",
+    IconSettingsEnum.setTimeData => "Иконка для ",
+    IconSettingsEnum.dropDown => "Иконка для ",
+    IconSettingsEnum.dropUp => "Иконка для ",
+    IconSettingsEnum.goBack => "Иконка для ",
+    IconSettingsEnum.textShort => "Иконка для ",
+    IconSettingsEnum.textMidl => "Иконка для ",
+    IconSettingsEnum.textFull => "Иконка для ",
+    IconSettingsEnum.textField => "Иконка для ",
+    IconSettingsEnum.slider => "Иконка для ",
+    IconSettingsEnum.themeLight => "Иконка для ",
+    IconSettingsEnum.themeDark => "Иконка для ",
+    IconSettingsEnum.themeSystem => "Иконка для ",
+    IconSettingsEnum.import => "Иконка для импорта",
+  };
+}
