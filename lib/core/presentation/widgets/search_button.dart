9d884a5fcb728d24b50ac1ec5764b87e9e9583cb
Sore Wa Nya <sorewanya@gmail.com>
2025-06-30 14:37:20 +0400

    add SearchButtonWidget

---
+++ lib/core/presentation/widgets/search_button.dart
@@ -0,0 +1,19 @@
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+import 'package:clear_app_helper/core/icons_helper.dart';
+import 'package:flutter/material.dart';
+
+class SearchButtonWidget extends StatelessWidget {
+  const SearchButtonWidget({super.key, required this.onPressed});
+
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        onPressed();
+      },
+      child: IconsHelper.getIconByEnum((IconSettingsEnum.searchSearch)),
+    );
+  }
+}
