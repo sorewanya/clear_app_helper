813b950e2eea5c72d7187b347be24b16e3764c68
Sore Wa Nya <sorewanya@gmail.com>
2025-06-30 15:30:14 +0400

    add ViewsPagesEmptyCheckerWidget

---
+++ lib/core/presentation/widgets/views_pages_empty_checker_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:clear_app_helper/core/domain/entities/search_entity.dart';
+import 'package:clear_app_helper/core/presentation/widgets/empty_list_widget.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_reset_text_and_icon_button.dart';
+
+/// Check [curentList] and [searchEntity] : [isEmpty]
+///
+/// if [curentListIsEmpty] true - add [EmptyListWidget],
+///
+/// if [searchEntity] not empty - add [SearchResetTextAndIconButton] with [resetSearch]
+class ViewsPagesEmptyCheckerWidget extends StatelessWidget {
+  const ViewsPagesEmptyCheckerWidget({
+    super.key,
+    required this.curentListIsEmpty,
+    required this.searchEntity,
+    required this.resetSearch,
+  });
+
+  /// take curentList.isEmpty
+  final bool curentListIsEmpty;
+
+  /// searchEntity used to take curentList
+  final SearchEntity searchEntity;
+
+  /// send to [SearchResetTextAndIconButton]
+  final Function()? resetSearch;
+
+  @override
+  Widget build(BuildContext context) {
+    if (curentListIsEmpty) {
+      return EmptyListWidget(onTap: resetSearch);
+    } else if (!searchEntity.isEmpty()) {
+      return SearchResetTextAndIconButton(onTap: resetSearch, openEndDrawer: true);
+    }
+    return const SizedBox();
+  }
+}
