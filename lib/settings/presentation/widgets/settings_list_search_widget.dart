394df2de32921a31b8aa62cb2abc3b2d60eab217
Sore Wa Nya <sorewanya@gmail.com>
2025-07-01 18:46:09 +0400

    add SettingsListSearchWidget

---
+++ lib/settings/presentation/widgets/settings_list_search_widget.dart
@@ -0,0 +1,152 @@
+import 'package:clear_app_helper/core/i18n/core_i18n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+import 'package:clear_app_helper/core/icons_helper.dart';
+
+import 'package:clear_app_helper/core/presentation/functions.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_case_sensitive_warning_widget.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_checkbox_widget.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_find_text_and_icon_button.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_reset_text_and_icon_button.dart';
+import 'package:clear_app_helper/core/presentation/widgets/search_text_form_field_with_reset_widget.dart';
+import 'package:clear_app_helper/settings/domain/entities/search/settings_search_entity.dart';
+import 'package:clear_app_helper/settings/domain/entities/settings_required_types.dart';
+import 'package:clear_app_helper/settings/presentation/bloc/settings_bloc_bloc.dart';
+import 'package:clear_app_helper/settings/presentation/widgets/saved_search_entity.dart';
+import 'package:get_it/get_it.dart';
+
+class SettingsListSearchWidget extends StatefulWidget {
+  final SettingsSearchEntity searchEntity;
+  const SettingsListSearchWidget({super.key, required this.searchEntity});
+
+  @override
+  State<SettingsListSearchWidget> createState() => _SettingsListSearchWidgetState();
+}
+
+class _SettingsListSearchWidgetState extends State<SettingsListSearchWidget> {
+  late SettingsSearchEntity _searchEntity;
+
+  TextEditingController idController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  late String dropdownSettingsRequiredTypeValue;
+
+  @override
+  void initState() {
+    _searchEntity = widget.searchEntity.copyWith();
+    idController.text = _searchEntity.id?.toString() ?? "";
+    nameController.text = _searchEntity.name ?? "";
+    dropdownSettingsRequiredTypeValue = "_";
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    idController.dispose();
+    nameController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final settingsBloc = context.read<SettingsBloc>();
+
+    void filterSearchResults() {
+      settingsBloc.add(SettingsBlocEvent.load(_searchEntity));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          SearchResetTextAndIconButton(
+            onTap: (() {
+              setState(() {
+                idController.clear();
+                nameController.clear();
+
+                _searchEntity = const SettingsSearchEntity();
+                FunctionsHelper.showResetUpdateInfoBarOrFilter(
+                  filterSearchResults: () {
+                    _searchEntity = const SettingsSearchEntity();
+                    filterSearchResults();
+                  },
+                );
+              });
+            }),
+          ),
+          const SizedBox(height: 10),
+          Builder(
+            builder: (context) {
+              return SavedSearchEntity<SettingsSearchEntity>(
+                setting: SettingsSettingsEnum.savedSearch,
+                fromJson: SettingsSearchEntity.fromJson,
+                searchEntity: _searchEntity,
+                setSearchEntity: (se) {
+                  settingsBloc.add(SettingsBlocEvent.load(se));
+                },
+                setState: (f) => setState(() => f()),
+              );
+            },
+          ),
+
+          ///NAME
+          SearchTextFormFieldResetResetWidget(
+            controller: nameController,
+            labelAndHintText: GetIt.instance<CoreI18n>().searchName,
+            filtr: filterSearchResults,
+            setSearchParam: (s) => _searchEntity = _searchEntity.copyWith(name: s),
+            setState: (f) => setState(() => f()),
+          ),
+
+          ///isDeleted
+          SearchCheckboxWidget(
+            filtr: filterSearchResults,
+            param: _searchEntity.isDeleted,
+            setParam: (b) => _searchEntity = _searchEntity.copyWith(isDeleted: b),
+            text: GetIt.instance<CoreI18n>().searchShowDeleted,
+            setState: (f) => setState(() => f()),
+          ),
+
+          ///SettingsRequiredType
+          DropdownButtonFormField<String>(
+            value: dropdownSettingsRequiredTypeValue,
+            icon: IconsHelper.getIconByEnum(IconSettingsEnum.dropDown),
+            elevation: 16,
+            items: [
+              const DropdownMenuItem<String>(value: "_", child: Text("_")),
+              ...SettingsRequiredTypesEnum.values.map(
+                (e) => DropdownMenuItem<String>(value: e.index.toString(), child: Text(e.name)),
+              ),
+            ],
+            onChanged: (value) => setState(() {
+              dropdownSettingsRequiredTypeValue = value ?? "_";
+              _searchEntity = _searchEntity.copyWith(confirmType: int.tryParse(dropdownSettingsRequiredTypeValue));
+            }),
+          ),
+
+          ///isChanged
+          SearchCheckboxWidget(
+            filtr: filterSearchResults,
+            param: _searchEntity.isChanged,
+            setParam: (b) => _searchEntity = _searchEntity.copyWith(isChanged: b),
+            text: GetIt.instance<CoreI18n>().searchShowChanged,
+            setState: (f) => setState(() => f()),
+          ),
+          const SizedBox(height: 10),
+          const SearchCaseSensitiveWarningWidget(),
+          const SizedBox(height: 10),
+          SearchFindTextAndIconButton(
+            onTap: (() {
+              setState(() {
+                filterSearchResults();
+              });
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
