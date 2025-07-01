e5795cb35382788a562d0d897340ccbddc4b3645
Sore Wa Nya <sorewanya@gmail.com>
2025-07-01 18:21:14 +0400

    add EntityBloc

---
+++ lib/core/presentation/bloc/entity_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:clear_app_helper/core/domain/entities/item_actions.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+///[Entity] is [AppEntity] or [ComplexObject] or any other if u can create bloc with this functions
+abstract class EntityBloc<BlocEvent, BlocState, Entity, SearchEntity> extends Bloc<BlocEvent, BlocState> {
+  EntityBloc(super.initialState);
+  Stream<Entity?> getStreamById(int id);
+  Stream<void> watchObjectLazy(int? id);
+  Future<Entity?> getById(int? itemId);
+  Stream<Entity> get(SearchEntity searchEntity);
+  ItemActions? itemActions;
+  void setItemActions(ItemActions? actions) => itemActions = actions;
+}
