69a472b449a38e7fcbc5656e1a27c5a276a4b673
Sore Wa Nya <sorewanya@gmail.com>
2025-07-01 19:00:34 +0400

    add NamedSearchEntity

---
+++ lib/settings/domain/entities/named_entity.dart
@@ -0,0 +1,7 @@
+import 'package:clear_app_helper/core/domain/entities/search_entity.dart';
+
+class NamedSearchEntity<Type extends SearchEntity> {
+  NamedSearchEntity(this.name, this.searchEntity);
+  final String name;
+  final Type searchEntity;
+}
