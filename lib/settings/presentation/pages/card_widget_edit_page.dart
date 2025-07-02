b931b5b1e6ce589090829dd2041eea71f619f9dc
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 15:28:06 +0400

    add CardWidgetEditPage

---
+++ lib/settings/presentation/pages/card_widget_edit_page.dart
@@ -0,0 +1,31 @@
+import 'package:clear_app_helper/core/i18n/core_i18n.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:clear_app_helper/core/presentation/widgets/my_scaffold_widget.dart';
+import 'package:clear_app_helper/core/presentation/widgets/settings_builder_widget.dart';
+import 'package:clear_app_helper/core/settings_route_names.dart';
+import 'package:clear_app_helper/settings/presentation/widgets/card_widget_edit_widget.dart';
+import 'package:get_it/get_it.dart';
+
+class CardWidgetEditPage extends StatelessWidget {
+  final Widget drawer;
+  const CardWidgetEditPage({super.key, required this.drawer});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> arg = Get.arguments ?? {};
+    final List<String> listOfNames = arg["list"] ?? [];
+    final String routeName = arg["routeName"] ?? SettingsRouteNames.settingsViewPage;
+
+    return Scaffold(
+      body: SettingsBuilderWidget(
+        childFunc: (sf) => MyScaffoldWidget(
+          appBarTitle: Text(GetIt.instance<CoreI18n>().settings),
+          body: CardWidgetEditWidget(listOfNames: listOfNames, routeName: routeName),
+          drawer: drawer,
+        ),
+      ),
+    );
+  }
+}
