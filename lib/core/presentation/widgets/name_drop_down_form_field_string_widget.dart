42186ed891293880ac82617a22bc76ff7bae1c9b
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 15:10:09 +0400

    add NameDropDownFormFieldStringWidget

---
+++ lib/core/presentation/widgets/name_drop_down_form_field_string_widget.dart
@@ -0,0 +1,67 @@
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+import 'package:clear_app_helper/core/icons_helper.dart';
+import 'package:flutter/material.dart';
+
+/// wrapper around [DropdownButtonFormField]\<String>
+class NameDropDownFormFieldStringWidget extends StatelessWidget {
+  const NameDropDownFormFieldStringWidget({
+    super.key,
+    required this.dropdownNameValue,
+    required this.setState,
+    required this.setDropdownNameValue,
+    required this.setDropdownNameValueId,
+    required this.curentMap,
+    this.setShouldPop,
+    this.withoutUnderline = false,
+  });
+
+  /// curent value
+  final String dropdownNameValue;
+
+  /// value variants name:id
+  final Map<String, int> curentMap;
+
+  /// callback new value
+  final Function(String s) setDropdownNameValue;
+
+  /// callback new value id
+  final Function(int id) setDropdownNameValueId;
+
+  final Function(bool b)? setShouldPop;
+
+  /// ```
+  /// setState:(f) => setState(() => f()),
+  /// ```
+  final Function(Function f) setState;
+
+  /// if true variant "_" not added
+  final bool withoutUnderline;
+
+  @override
+  Widget build(BuildContext context) {
+    List<DropdownMenuItem<String>> dropdownList = [];
+
+    curentMap.forEach((key, value) => dropdownList.add(DropdownMenuItem<String>(value: key, child: Text(key))));
+    return DropdownButtonFormField<String>(
+      value: dropdownNameValue,
+      icon: IconsHelper.getIconByEnum((IconSettingsEnum.dropDown)),
+      elevation: 16,
+      items: [
+        if (!withoutUnderline) const DropdownMenuItem<String>(value: "_", child: Text("_")),
+        ...dropdownList,
+      ],
+      onChanged: (newValue) {
+        setState(() {
+          if (newValue == dropdownNameValue) return;
+          if (setShouldPop != null) setShouldPop!(false);
+          if (newValue != null) {
+            setDropdownNameValue(newValue);
+            if (newValue != "_") {
+              setDropdownNameValueId(curentMap[newValue]!);
+            }
+          }
+        });
+      },
+    );
+  }
+}
