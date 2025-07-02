2491f7284ae92b7de2e1dc6ebe1d4366a168ef4b
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 18:22:35 +0400

    add TableCellSlidableActionOpen

---
+++ lib/core/presentation/widgets/table_cell_slidable_action_open.dart
@@ -0,0 +1,30 @@
+import 'package:clear_app_helper/core/icons_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TableCellSlidableActionOpen extends StatelessWidget {
+  final bool right;
+  const TableCellSlidableActionOpen({super.key, this.right = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCell(
+      verticalAlignment: TableCellVerticalAlignment.fill,
+      child: GestureDetector(
+        onTap: () => right ? Slidable.of(context)?.openEndActionPane() : Slidable.of(context)?.openStartActionPane(),
+        child: Align(
+          alignment: Alignment.center,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColorLight.withValues(alpha: 0.3),
+              borderRadius: right
+                  ? const BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10))
+                  : const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
+            ),
+            child: Icon(IconsHelper.getIconDataOrNullByString(right ? ">" : "<")),
+          ),
+        ),
+      ),
+    );
+  }
+}
