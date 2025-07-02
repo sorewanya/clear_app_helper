236295953e6e068454e94c1b30c6d2eae22a89e0
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 11:16:35 +0400

    add DetailPage

---
+++ lib/core/presentation/pages/detail_page.dart
@@ -0,0 +1,35 @@
+import 'package:clear_app_helper/core/domain/entities/search_entity.dart';
+import 'package:clear_app_helper/core/presentation/bloc/entity_bloc.dart';
+import 'package:clear_app_helper/core/presentation/functions.dart';
+import 'package:clear_app_helper/core/presentation/widgets/loading_indicator.dart';
+import 'package:clear_app_helper/core/presentation/widgets/settings_builder_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class DetailPage<Entity, EBloc extends EntityBloc<dynamic, dynamic, Entity, SE>, SE extends SearchEntity>
+    extends StatelessWidget {
+  const DetailPage({super.key, required this.loadString, required this.child, required this.getIt});
+
+  final String loadString;
+  final Widget Function(Entity entity) child;
+  final GetIt getIt;
+
+  @override
+  Widget build(BuildContext context) {
+    return SettingsBuilderWidget(
+      childFunc: (sf) {
+        final itemBloc = getIt<EBloc>();
+        final se = FunctionsHelper.getArgs<SE>() ?? getIt<SE>();
+        return StreamBuilder<Entity>(
+          stream: itemBloc.get(se),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData && snapshot.data == null) {
+              return loadingIndicator(loadString);
+            }
+            return child(snapshot.data as Entity);
+          },
+        );
+      },
+    );
+  }
+}
