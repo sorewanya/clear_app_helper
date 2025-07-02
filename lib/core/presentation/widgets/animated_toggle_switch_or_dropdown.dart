8a4f30d88cc02f4cd18fc1286693f7e4d24f7cc4
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 11:24:22 +0400

    add AnimatedToggleSwitchOrDropdown

---
+++ lib/core/presentation/widgets/animated_toggle_switch_or_dropdown.dart
@@ -0,0 +1,68 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+import 'package:clear_app_helper/core/icons_helper.dart';
+import 'package:flutter/material.dart';
+
+/// AnimatedToggleSwitch, if all values have icon from
+/// (```IconsHelper.getIconDataOrNullByString```)
+///
+/// else DropdownButtonFormField with this values
+class AnimatedToggleSwitchOrDropdown extends StatelessWidget {
+  /// curent Value
+  final String value;
+
+  final List<String>? values;
+
+  /// true if in userValue/defaultValue save index of value
+  final bool hasIndexValue;
+
+  /// new value callback
+  final Function(String? s) setValue;
+
+  /// ```
+  /// setState: (f) => setState(() => f()),
+  /// ```
+  final Function(Function f) setState;
+  const AnimatedToggleSwitchOrDropdown({
+    super.key,
+    required this.value,
+    required this.values,
+    required this.setValue,
+    required this.setState,
+    required this.hasIndexValue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final asInt = int.tryParse(value);
+    if (values == null || (hasIndexValue == true && asInt == null)) {
+      return const SizedBox();
+    }
+
+    bool haveAllIcons = true;
+    final iconDatas = values!.map((e) => IconsHelper.getIconDataOrNullByString(e)).toList();
+    if (iconDatas.contains(null)) haveAllIcons = false;
+    final curentValue = hasIndexValue == true ? values![asInt!] : value;
+    return haveAllIcons
+        ? AnimatedToggleSwitch<String>.rolling(
+            current: curentValue,
+            values: values!,
+            onChanged: (i) => setState(() => setValue(hasIndexValue == true ? values!.indexOf(i).toString() : i)),
+            iconBuilder: ((value, foreground) {
+              return Icon(IconsHelper.getIconDataByString(value));
+            }),
+          )
+        : DropdownButtonFormField<String>(
+            value: curentValue,
+            icon: IconsHelper.getIconByEnum((IconSettingsEnum.dropDown)),
+            elevation: 16,
+            items: values!.map((e) {
+              return DropdownMenuItem<String>(value: e, child: Text(e));
+            }).toList(),
+            onChanged: ((value) {
+              final newValue = hasIndexValue == true ? values!.indexOf(value!).toString() : value!;
+              setState(() => setValue(newValue));
+            }),
+          );
+  }
+}
