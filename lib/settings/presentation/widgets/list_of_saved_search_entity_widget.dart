3ef3b701d1b5c897b852f5d95edce8ae3ab4fb71
Sore Wa Nya <sorewanya@gmail.com>
2025-07-02 11:15:47 +0400

    add ListOfSavedSearchEntityWidget

---
+++ lib/settings/presentation/widgets/list_of_saved_search_entity_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:clear_app_helper/core/domain/entities/settings_enum.dart';
+import 'package:clear_app_helper/core/icons_helper.dart';
+
+class IndexedString {
+  final int index;
+  final String str;
+  IndexedString({required this.index, required this.str});
+}
+
+class ListOfSavedSearchEntityWidget extends StatefulWidget {
+  final List<String> values;
+  final Function(List<String> values) updateValues;
+
+  const ListOfSavedSearchEntityWidget({super.key, required this.values, required this.updateValues});
+
+  @override
+  State<ListOfSavedSearchEntityWidget> createState() => _ListOfSavedSearchEntityWidgetState();
+}
+
+class _ListOfSavedSearchEntityWidgetState extends State<ListOfSavedSearchEntityWidget> {
+  List<IndexedString> list = [];
+  @override
+  void initState() {
+    for (int index = 0; index < widget.values.length; index += 1) {
+      list.add(IndexedString(index: index, str: widget.values[index]));
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    list.clear();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withValues(alpha: 0.05);
+    final Color evenItemColor = colorScheme.primary.withValues(alpha: 0.15);
+    void updateUserValue() {
+      widget.updateValues(list.map((e) => e.str).toList());
+    }
+
+    return SizedBox(
+      height: 400,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).primaryColorLight.withValues(alpha: 0.3)),
+          borderRadius: BorderRadius.circular(10),
+          color: Theme.of(context).primaryColor.withValues(alpha: 0.6),
+        ),
+        child: ReorderableListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              key: Key('$index'),
+              tileColor: index.isOdd ? oddItemColor : evenItemColor,
+              title: Row(
+                children: [
+                  Text(list[index].str),
+                  IconButton(
+                    onPressed: () => setState(() {
+                      list.removeAt(index);
+                      updateUserValue();
+                    }),
+                    icon: IconsHelper.getIconByEnum(IconSettingsEnum.delete),
+                  ),
+                ],
+              ),
+            );
+          },
+          onReorder: (int oldIndex, int newIndex) => setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final IndexedString item = list.removeAt(oldIndex);
+            list.insert(newIndex, item);
+            updateUserValue();
+          }),
+        ),
+      ),
+    );
+  }
+}
