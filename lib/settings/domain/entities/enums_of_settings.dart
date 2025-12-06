57b77df3db96c06c52e49f86b86511b5f4894b5e
Sore Wa Nya <sorewanya@gmail.com>
2025-12-06 11:58:14 +0400

    update pubspec.yaml ; add country_icons to AnimatedToggleSwitch; add quick fastHash getter

--- lib/settings/domain/entities/enums_of_settings.dart
+++ lib/settings/domain/entities/enums_of_settings.dart
@@ -1,7 +1,9 @@
+import 'package:clear_app_helper/core/hash_func.dart';
 import 'package:clear_app_helper/settings/domain/entities/settings_types.dart';
 
 mixin EnumsOfSettings {
   String get description;
+  int get getFastHash => fastHash(name);
   String get name;
   SettingsTypeEnum get type;
   int get typeIndex => type.index;
