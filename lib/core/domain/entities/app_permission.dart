805ef127b743905b2947f8b9d4ef13a4bcbe3cbf
Sore Wa Nya <sorewanya@gmail.com>
2025-12-17 19:07:54 +0400

    add canWriteToDirectory

--- lib/core/domain/entities/app_permission.dart
+++ lib/core/domain/entities/app_permission.dart
@@ -1,10 +1,29 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:permission_handler/permission_handler.dart';
 
 abstract class AppPermission {
-  Future<PermissionStatus?> request();
+  Future<bool> canWriteToDirectory(String directoryPath);
+  Future<PermissionStatus?> storageRequest();
 }
 
 class AppPermissionImpl implements AppPermission {
   @override
-  Future<PermissionStatus?> request() => Permission.storage.request();
+  Future<bool> canWriteToDirectory(String directoryPath) async {
+    final testFile = File(
+      '$directoryPath${Platform.pathSeparator}.write_test_${DateTime.now().millisecondsSinceEpoch}',
+    );
+    try {
+      await testFile.writeAsString('test');
+      await testFile.delete();
+      return true;
+    } catch (e) {
+      log('Cannot write to $directoryPath: $e');
+      return false;
+    }
+  }
+
+  @override
+  Future<PermissionStatus?> storageRequest() => Permission.storage.request();
 }
